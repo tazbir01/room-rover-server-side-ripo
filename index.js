730226337e493af18a0cd488f91b4d516b7abdcd
tazbir01 <tajberahamad@gmail.com>
2023-11-13 16:52:58 +0600

    added get method for reviews

--- index.js
+++ index.js
@@ -90,6 +90,12 @@ async function run() {
       res.send(result)
     })
 
+    app.get("/reviews", async(req, res)=>{
+      const cursor = reviewCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
